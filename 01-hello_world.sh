098b658f7aafc33e9c7cf7f53b79e1028cc75554
Elrey <eddygarcia06@gmail.com>
2020-11-22 15:34:12 -0500

    new message v2

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+echo "Hello World bitchess"
+
+
+-e  echo "1/n2/n3" " is the name bitch "
+
+read -p "whats your name bitch" name 
+
+
+echo -e "okay  $name are you a little bitchor a badd asss bitch "
