76a04c21812839f2e907959097d1fb5068090100
elperrrucho <38382504+elperrrucho@users.noreply.github.com>
2020-11-24 12:12:30 -0500

    Update 03-calculs.sh

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+echo 10+10 #affiche 10+10
+echo $((10+10)) #affiche 20
+let resultat=10*10 #si on écrit let devant la variable qu'on assigne, 
+		   #agit comme un $(())
+echo $resultat
+toto=10*10
+echo $toto
+
+echo "le résutalt de $1 - $2 = $(($1-$2))"
