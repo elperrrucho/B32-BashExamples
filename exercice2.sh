f9e2600775f471f9ce263c18db498c6cb5e6fe2d
elperrrucho <38382504+elperrrucho@users.noreply.github.com>
2020-11-24 12:16:02 -0500

    Update exercice2.sh

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+afficherMenu() {
+	echo "===================="
+	echo " d = dictionnaire"
+	echo " q = quitter"
+	echo "===================="
+}
+
+resultat='z'
+
+while test $resultat != "q"
+do
+	afficherMenu
+	read -p "Entrez votre choix : " resultat
+	if test $resultat == "d"
+	then
+		./exercice1.sh
+	fi
+done
+© 2020 GitHub, Inc.
