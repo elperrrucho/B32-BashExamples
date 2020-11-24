e12cede8169482e9188bdd22d402705f2590e740
elperrrucho <38382504+elperrrucho@users.noreply.github.com>
2020-11-24 12:13:01 -0500

    Update 04-condition.sh

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,45 @@
 #!/bin/bash
 
+note=$1
+#comparaison int
+#-lt <, -gt >, -ge >=, -le <=, -eq ==, -ne !=
+if [[ $note -lt 60 ]]
+then
+	echo "echec"
+elif test $note -eq 60; then #On peut utiliser un ; pour finir la ligne
+	echo "C'est limite !"
+else 
+	echo "Excellent !"
+fi
 
+read lettre
 
+# case et les classes de caractères
+# fonctionne pour une seule lettre
+case $lettre in
+    c)
+        echo "C'est c"
+        ;;
+    d)
+        echo "C'est d"
+        ;;
+    [1-8])
+        echo "C'est un chiffre entre 1 et 8"
+        ;;
+    [[:lower:]])
+        echo "minusule"
+        ;;
+    [[:upper:]])
+        echo "majuscule"
+        ;;
+        *)
+        echo "Pas une lettre"
+        ;;
+esac
 
-
-
-
-
+#comparaison string
+# ici on peut utiliser les == et !=
+if test $lettre != "a" #!= et == fonctionne avec les strings
+then
+    echo "la lettre n’est pas a"
+fi
