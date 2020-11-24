6a1755ae6a754665627e2be2b846ef0a71134e5d
elperrrucho <38382504+elperrrucho@users.noreply.github.com>
2020-11-24 12:16:23 -0500

    Update exercice3.sh

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+faireEspaces() {
+        for ((i=0;i<$1;i++))
+        do
+                echo -n " "
+        done
+}
+
+max=30
+
+for ((i=0;i<=$max;i++))
+do
+        clear
+        faireEspaces $i
+        echo "@"
+        sleep 0.2
+done
+
+clear
+faireEspaces $max
+echo "BOOM"
+sleep 2
+clear
