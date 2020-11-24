d0f55085e27a2205ac144abff0f0e67a4bf1553d
elperrrucho <38382504+elperrrucho@users.noreply.github.com>
2020-11-24 12:14:11 -0500

    Update 07-fonctions.sh

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+maFonction() {
+	echo "mon parametre a la valeur $1, et lautre la valeur $2"
+}
+maFonction patate poutine
+# res mon parametre a la valeur pirate, et lautre la valeur poutine
+
+afficherInfo() {
+	echo "-------------------------------"
+	echo "kernel : " `uname -rs`
+	echo "-------------------------------"
+}
+#appel une fonction sans parametres
+afficherInfo
