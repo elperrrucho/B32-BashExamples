0b053a2d8d06db0d3ad874c96f8b03a6fb8a929e
elperrrucho <38382504+elperrrucho@users.noreply.github.com>
2020-11-24 12:14:58 -0500

    Update 09-fichiers.sh

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+
+#tests sur les fichiers
+if test -e $1
+then
+        echo "existe"
+else
+        echo "inexistant"
+fi
+
+#Sur les fichiers
+#test  -e  fichier		vrai si fichier existe
+#test  -d  fichier		vrai si fichier est un répertoire
+#Sur les chaînes de caractères
+#test  -z  chaine		vrai si chaine est de taille nulle
+#test  -n  chaine		vrai si chaine n'est pas de taille nulle
