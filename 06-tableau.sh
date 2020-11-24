9d4fabdd14a8f0a5b7780fde35dbe360eb1a78aa
elperrrucho <38382504+elperrrucho@users.noreply.github.com>
2020-11-24 12:13:46 -0500

    Update 06-tableau.sh

--- 06-tableau.sh
+++ 06-tableau.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+declare -a array=(Back To The Future)
+echo ${array[0]}	# Ceci retourne également le premier élément
+echo ${array[1]}	# Ceci retourne le deuxième élément
+echo ${array[2]}	# Ceci retourne le troisième élément
+
+echo ${#array[@]} # La taille du tableau
+
+declare -a arr=(1 2 3 4 5 6)
+
+
+for ((i=0; i < ${#arr[@]};i++)); do
+	echo $(( ${arr[$i]} ))
+done
